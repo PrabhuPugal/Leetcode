bf52619c23b7a9d2c4b6cda124bf58c132b751a3
Prabhu P <prabhupugal01@gmail.com>
2025-12-17 00:37:50 -0700

    Time: 51 ms (90.32%), Space: 18.1 MB (21.04%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        nums2=[]
+        #nums2=[]
         #i=0
         #j=n
         #for k in range(n):
@@ -9,8 +9,9 @@ class Solution:
           #  i+=1
            # j+=1
         #or you could also do this
+        res = []
         for i in range(n):
-            nums2=nums2+[nums[i]]
-            nums2=nums2+[nums[n+i]]
-        return nums2
+            res+=[nums[i]]
+            res+=[nums[i+n]]
+        return res
 
